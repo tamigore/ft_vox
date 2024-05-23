c68b957684f9fd7c143f983521e08fcf0a8de396
Tadeo AMIGORENA <tamigore@e1r12p14.clusters.42paris.fr>
2024-05-23 11:27:54 +0200

    42try fix

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -415,9 +415,6 @@ GLFWwindow*	glfw_init_window(void)
 	// enable modes
 	glEnable(GL_DEPTH_TEST);
 
-	// glEnable(GL_BLEND);
-	// glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	// glDisable(GL_CULL_FACE);
 	return window;
 }
 
